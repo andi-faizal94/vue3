fe89abf50c07deb59fdb9946da3e055a4954a2de
andi-faizal94 <andifaizal647@gmail.com>
2021-03-12 19:35:30 +0700

    belajar vue 3

---
+++ src/components/Todo.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h3>Todolist</h3>
+
+    <h6 v-for="(todo, index) in todos" :key="index">{{todo}}</h6>
+    <p>{{todos.task}}</p>
+    <input v-model="todos.task" placeholder="text" />
+    <input type="button" value="button" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Todo",
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          task: ""
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped>
+</style>
